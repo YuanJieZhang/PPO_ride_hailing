e51a0392d8204145d0b9fc46c9aba1217891d19e
YuanJieZhang <970528347@qq.com>
2024-04-08 00:46:27 +0800

    init

---
+++ data/sample/create_city_state.py
@@ -0,0 +1,43 @@
+"""
+This class implements a job for creating city state
+"""
+
+import logging
+
+
+class StateCreator(object):
+    """
+    This class implements a job for creating city state
+    """
+
+    def __init__(self, DATA_DIR, DATA_DIR_origin):
+        """
+        Constructor
+        :param DATA_DIR:
+        :return:
+        """
+        self.DATA_DIR = DATA_DIR
+        self.DATA_DIR_origin = DATA_DIR_origin
+        self.logger = logging.getLogger("logger")
+
+    def run(self):
+        """
+        This method executes the job
+        :param:
+        :return:
+        """
+        self.logger.info("Starting job: StateCreator\n")
+
+        city_state_creator = CityStateCreator(self.DATA_DIR, self.DATA_DIR_origin)
+        city_state = city_state_creator.get_city_states()
+        self.logger.info("Exporting city states\n")
+        data_exporter = DataExporter(self.DATA_DIR)
+        data_exporter.export_city_state(city_state)
+
+        self.logger.info("Finished job: CreateCityStateJob")
+
+if __name__ == "__main__":
+    DATA_DIR = "./data/"
+    DATA_DIR_origin = "./data/raw.csv"
+    job = CreateCityStateJob(DATA_DIR, DATA_DIR_origin)
+    job.run()
